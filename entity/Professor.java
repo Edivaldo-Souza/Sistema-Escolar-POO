12f27c3c58f8563c960d45be386b0980fbc3b24c
jlucasbx <jlucasb16@gmail.com>
2022-09-02 18:25:24 -0300

    implementado classe professor

--- entity/Professor.java
+++ entity/Professor.java
@@ -1,25 +1,65 @@
 class Professor{
-	
-	String nome;
-	Endereco endereco;
-	String cpf;
-	Turmas[] turmas;
+
+	private String nome;
+	private Endereco endereco;
+	private String cpf;
+	private Turmas[] turmas;
 
 
 	Professor(String nome,String cpf){
+		this.setCpf(cpf);
+		this.setEndereco(endereco);
+		this.setNome(nome);
 	}
-	void cadastrar(Database db){
-	
 
+
+	public void setNome(String nome) {
+		if(this.nome.isEmpty())
+		System.out.prinln("Nome invalido");
+		else
+		this.nome = nome;
+
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+
+	}
+
+	public void setCpf(String cpf) {	
+		if(this.cpf.isEmpty())
+		System.out.prinln("Nome invalido");
+		else
+		this.cpf = cpf;
+
+	}
+
+	public String getCpf() {
+		return this.cpf;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public Endereco getEndereco() {
+		return this.endereco;
+	}
+
+	public Turmas[] getTurmas() {
+		return this.turmas;
+	}
+
+	void cadastrar(Professor novo){
+		//Inserir no banco de dados
 	}
 
 	void deletar(){
-
-
+		//Deletar no banco de dados
 	}
 
 	void editar(){
-
+		//Editar no banco de dados
 
 	}
 
