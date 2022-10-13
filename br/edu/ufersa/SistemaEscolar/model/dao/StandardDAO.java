e9dcb2b58d490404edad0f164fb468e26aed9ea2
Edivaldo-Souza <edivaldoraimundo84.gmail.com>
2022-10-13 14:04:39 -0300

    merge StandardDAO

--- br/edu/ufersa/SistemaEscolar/model/dao/StandardDAO.java
+++ br/edu/ufersa/SistemaEscolar/model/dao/StandardDAO.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class StandardDAO<type> implements InterfaceDAO<type> {
-	 Connection connection;
+	Connection connection;
 	
 	synchronized public Connection getConnection() {
 		if(connection == null) {
